2364efee89a05072c04ca5b3798eb2561144a91e
ilia <i.makarov@andersenlab.com>
2020-12-20 14:03:27 +0200

    lifecircle method: *mounted*

--- src/components/app/NavBar.vue
+++ src/components/app/NavBar.vue
@@ -8,32 +8,16 @@
         <span class="black-text">12.12.12</span>
       </div>
 
-      <ul class="right hide-on-small-and-down">
-        <li>
-          <a
-            class="dropdown-trigger black-text"
-            href="#"
-            data-target="dropdown"
-          >
-            USER NAME
-            <i class="material-icons right">arrow_drop_down</i>
-          </a>
-
-          <ul id='dropdown' class='dropdown-content'>
-            <li>
-              <a href="#" class="black-text">
-                <i class="material-icons">account_circle</i>Профиль
-              </a>
-            </li>
-            <li class="divider" tabindex="-1"></li>
-            <li>
-              <a href="#" class="black-text">
-                <i class="material-icons">assignment_return</i>Выйти
-              </a>
-            </li>
-          </ul>
-        </li>
-      </ul>
+      <ProfileMenu/>
     </div>
   </nav>
 </template>
+
+<script>
+import ProfileMenu from './ProfileMenu.vue';
+
+export default {
+  name: 'NavBar',
+  components: { ProfileMenu },
+};
+</script>
---
+++ src/components/app/ProfileMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <ul class="right hide-on-small-and-down">
+    <li>
+      <a
+        class="dropdown-trigger black-text"
+        href="#"
+        data-target="dropdown"
+        ref="profileMenu"
+      >
+        USER NAME
+        <i class="material-icons right">arrow_drop_down</i>
+      </a>
+
+      <ul id='dropdown' class='dropdown-content'>
+        <li>
+          <router-link to="/profile" class="black-text">
+            <i class="material-icons">account_circle</i>Профиль
+          </router-link>
+        </li>
+        <li class="divider" tabindex="-1"></li>
+        <li>
+          <a href="#" class="black-text" @click.prevent="logout">
+            <i class="material-icons">assignment_return</i>Выйти
+          </a>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'ProfileMenu',
+  mounted() {
+    // eslint-disable-next-line
+    M.Dropdown.init(this.$refs.profileMenu, {
+      constrainWidth: false,
+    });
+  },
+  methods: {
+    logout() {
+      this.$router.push('/login?message=logout');
+    },
+  },
+};
+</script>
