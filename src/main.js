60b31a9c79c2936f8e24ece4ffc227df7b00a330
ilia <i.makarov@andersenlab.com>
2021-01-04 14:16:36 +0200

    connext firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import Vuelidate from 'vuelidate';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -14,8 +17,23 @@ Vue.use(Vuelidate);
 Vue.use(messagePlugin);
 Vue.filter('date', dateFilter);
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app');
+firebase.initializeApp({
+  apiKey: 'AIzaSyACruKId9nAbgFuXQA9hOKGCTWw3lviqEc',
+  authDomain: 'vue-study-adb4a.firebaseapp.com',
+  projectId: 'vue-study-adb4a',
+  storageBucket: 'vue-study-adb4a.appspot.com',
+  messagingSenderId: '754822022250',
+  appId: '1:754822022250:web:7cd11608e7b9c5f55ef613',
+});
+
+let app;
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app');
+  }
+});
