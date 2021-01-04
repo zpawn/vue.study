b03dd4f0af55f756e30d92ff39a02d807af01085
ilia <i.makarov@andersenlab.com>
2021-01-04 15:26:32 +0200

    register new user

--- src/store/auth.js
+++ src/store/auth.js
@@ -7,10 +7,30 @@ export default {
       try {
         await firebase.auth().signInWithEmailAndPassword(email, password);
       } catch (e) {
-        console.log('>>> store:auth');
+        console.log('>>> store:auth:login:error');
         throw e;
       }
     },
+
+    async register({ dispatch }, { name, email, password }) {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+        const uid = await dispatch('getUid');
+        await firebase.database().ref(`/users/${uid}/info`).set({
+          bill: 10000,
+          name,
+        });
+      } catch (e) {
+        console.log('>>> store:auth:register:error', e);
+        throw e;
+      }
+    },
+
+    getUid() {
+      const user = firebase.auth().currentUser;
+      return user ? user.uid : null;
+    },
+
     async logout() {
       await firebase.auth().signOut();
     },
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -100,7 +100,7 @@ export default {
     agree: { checked: (v) => v },
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -109,10 +109,15 @@ export default {
       const formData = {
         email: this.email,
         password: this.password,
+        name: this.name,
       };
 
-      console.log('>>> register:submit:api', formData);
-      this.$router.push('/');
+      try {
+        await this.$store.dispatch('register', formData);
+        this.$router.push('/');
+      } catch (e) {
+        // ...
+      }
     },
   },
 };
